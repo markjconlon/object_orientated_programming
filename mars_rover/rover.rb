64be1f7c30d80aae9d7d68f43b7160c1fe1279dd
Mark <markjconlon@gmail.com>
2017-03-29 20:23:04 -0400

    rover works

--- mars_rover/rover.rb
+++ mars_rover/rover.rb
@@ -8,21 +8,63 @@ class Rover
 
   def read_instruction(string)
     if string.is_a?(String)
-
+      command_array = string.upcase().split("")
+      command_array.each do |letter|
+        if letter == "M"
+          move()
+        elsif letter == "L"
+          turn("L")
+        elsif letter == "R"
+          turn("R")
+        else
+          puts "Oops looks like you used a character other than M, R, L"
+        end
+      end
+      puts location()
     else
       puts "You must provide strings only [M] = move [R] = rotate 90 degrees to the right [L] = rotate 90 degrees to the left you can provide more than one letter at a time without spaces"
     end
   end
 
   def move
+    if @direction == "N"
+      @y_coordinate += 1
+    elsif @direction == "S"
+      @y_coordinate -= 1
+    elsif @direction == "E"
+      @x_coordinate += 1
+    elsif @direction == "W"
+      @x_coordinate -= 1
+    end
 
   end
 
   def turn(direction)
+    if direction == "R"
+      if @direction == "N"
+        @direction = "E"
+      elsif @direction == "S"
+        @direction = "W"
+      elsif @direction == "E"
+        @direction = "S"
+      elsif @direction == "W"
+        @direction = "N"
+      end
 
+    else
+      if @direction == "N"
+        @direction = "W"
+      elsif @direction == "S"
+        @direction = "E"
+      elsif @direction == "E"
+        @direction = "N"
+      elsif @direction == "W"
+        @direction = "S"
+      end
+    end
   end
 
   def location
-    return "#{@x_coordinate} #{@y_coordinate} #{@direction}"
+    return "Your current location is #{@x_coordinate} #{@y_coordinate} #{@direction}"
   end
 end
